f0f564aedf8c7c4362f806c69330832deee676af
mabdullahdawod <120252427+mabdullahdawod@users.noreply.github.com>
2023-02-24 15:55:32 +0500

    Update 10diagonal.js

--- 10diagonal.js
+++ 10diagonal.js
@@ -30,7 +30,7 @@ const diag=(arr)=> {
     } 
     console.log( (rightDiag-leftDiag));
   }
-  let arr=[[11,2,4],[4,5,6],[10,,-12]];
+  let arr=[[11,2,4],[4,5,6],[10,8,-12]];
   y=diag(arr);
   console.log(y);
   
